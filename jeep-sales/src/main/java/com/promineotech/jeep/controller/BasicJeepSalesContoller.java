a0cd105ebaafd5ed1f48632e3b9aa1bc1f4c5c11
zacha <zachuff@yahoo.com>
2023-02-18 15:53:15 -0700

    first commit

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/controller/BasicJeepSalesContoller.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.promineotech.jeep.entity.Jeep;
+
+@RestController
+public class BasicJeepSalesContoller implements JeepSalesController {
+
+	@Override
+	public List<Jeep> fetchJeeps(String model, String trim) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
